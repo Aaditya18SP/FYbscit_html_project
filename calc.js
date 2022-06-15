90d184e150dc842b74a0a9e35d7651d97035f267
Aaditya18SP <107549181+Aaditya18SP@users.noreply.github.com>
2022-06-15 16:47:54 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,19 @@
+
+function bmi(){
+    var num3;
+    var num1=document.getElementById("num1").value;
+    var num2=document.getElementById("num2").value;
+    num3=num1/(num2*num2);
+    document.getElementById("ans").value=num3;
+    }
+    
+    function thr(){
+    var num12;
+    var num10=document.getElementById("num10").value;
+   
+    num12=220-num10;
+    document.getElementById("num12").value=num12;
+    
+    
+    }
+    
